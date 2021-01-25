c701646c04a883706e14efe091044b4bba2c6083
huaxueyihao <837403116@qq.com>
2021-01-26 07:38:50 +0800

    toutiao element-ui & axios

--- toutiao-pushlish-admin/src/router/index.js
+++ toutiao-pushlish-admin/src/router/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-
 Vue.use(VueRouter)
 
 const routes = [
- 
 ]
 
 const router = new VueRouter({
---
+++ toutiao-pushlish-admin/src/utils/request.js
@@ -0,0 +1,14 @@
+/**
+ * 基于 axios 请求模块
+ */
+
+import axios from 'axios'
+
+// 创建一个axios实例，说白了就是绘制了一个axios
+//
+const request = axios.create({
+    baseURL: 'http://ttapi.research.itcast.cn/'
+})
+
+
+
