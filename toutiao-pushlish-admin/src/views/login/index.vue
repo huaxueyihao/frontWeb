d961c3a125ec1f235c8fc6f254299348792074bd
huaxueyihao <837403116@qq.com>
2021-01-27 22:45:25 +0800

    toutiao login

--- toutiao-pushlish-admin/src/views/login/index.vue
+++ toutiao-pushlish-admin/src/views/login/index.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="login-container">
-    <el-form ref="user" :model="form" label-width="80px">
-      <el-form-item label="手机号">
-        <el-input v-model="user.mobile"></el-input>
-      </el-form-item>
-      <el-form-item label="验证码">
-        <el-input v-model="user.code"></el-input>
+    <el-form class="login-form" ref="form" :model="user" >
+      <el-form-item>
+        <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button>取消</el-button>
+        <el-input v-model="user.code" placeholder="请输入验证码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-checkbox v-model=checked>我已阅读并同意用户协议和隐私条款</el-checkbox>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="login-btn" type="primary" :loading="loginLoading" @click="onLogin">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+import request from '@/utils/request'
+
 export default {
   name: 'LoginIndex',
   data () {
@@ -23,16 +27,61 @@ export default {
       user: {
         mobile: '',
         code: ''
-      }
+      },
+      checked: false,
+      loginLoading: false
     }
   },
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    onLogin () {
+      // 获取表单数据
+      const user = this.user
+      // 表单数据
+
+      // 验证通过
+      this.loginLoading = true
+      request({
+        method: 'POST',
+        url: '/mp/v1_0/authorizations',
+        data: user
+      }).then(res => {
+        console.log(res)
+        this.$message({
+          message: '登录成功',
+          type: 'success'
+        })
+        this.loginLoading = false
+      }).catch(err => {
+        console.log('登录失败', err)
+        this.$message.error('登录失败，手机号或验证码错误')
+        this.loginLoading = false
+      })
     }
   }
 }
 </script>
 
 <style>
+.login-container {
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #857f7f;
+}
+
+.login-form {
+  background-color: #fff;
+  padding: 50px;
+  min-width: 300px;
+}
+
+.login-btn {
+  width: 100%;
+}
+
 </style>
