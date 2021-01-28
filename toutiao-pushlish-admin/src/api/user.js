570c670b1843e2fb1735b9c6ad9add8874e3c1e2
huaxueyihao <837403116@qq.com>
2021-01-28 23:06:32 +0800

    toutiao login module

---
+++ toutiao-pushlish-admin/src/api/user.js
@@ -0,0 +1,9 @@
+import request from '@/utils/request'
+// 用户登录
+export const login = data => {
+  return request({
+    method: 'POST',
+    url: '/mp/v1_0/authorizations',
+    data
+  })
+}
--- toutiao-pushlish-admin/src/views/login/index.vue
+++ toutiao-pushlish-admin/src/views/login/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="login-container">
-    <el-form class="login-form" ref="form" :model="user" >
-      <el-form-item>
+    <el-form class="login-form" ref="login-form" :model="user" :rules="formRules" >
+      <el-form-item prop="mobile">
         <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
       </el-form-item>
-      <el-form-item>
+      <el-form-item prop="code">
         <el-input v-model="user.code" placeholder="请输入验证码"></el-input>
       </el-form-item>
-      <el-form-item>
-        <el-checkbox v-model=checked>我已阅读并同意用户协议和隐私条款</el-checkbox>
+      <el-form-item prop="agree">
+        <el-checkbox v-model="user.agree">我已阅读并同意用户协议和隐私条款</el-checkbox>
       </el-form-item>
       <el-form-item>
         <el-button class="login-btn" type="primary" :loading="loginLoading" @click="onLogin">登录</el-button>
@@ -18,45 +18,73 @@
 </template>
 
 <script>
-import request from '@/utils/request'
+import { login } from '@/api/user'
 
 export default {
   name: 'LoginIndex',
   data () {
     return {
       user: {
-        mobile: '',
-        code: ''
+        mobile: '13911111111',
+        code: '123456',
+        agree: true
       },
       checked: false,
-      loginLoading: false
+      loginLoading: false,
+      formRules: {
+        mobile: [
+          { required: true, message: '手机号不能为空', trigger: 'change' },
+          { pattern: /^1[3|5|7|8|9]\d{9}/, message: '请输入正确的号码格式', trigger: 'change' }
+        ],
+        code: [
+          { required: true, message: '验证码不能为空', trigger: 'change' }
+        ],
+        agree: [
+          {
+            // 自定义校验规则
+            validator: (rule, value, callback) => {
+              if (value) {
+                callback()
+              } else {
+                callback(new Error('请勾选同意用户协议'))
+              }
+            },
+            // message: '请勾选同意用户协议',
+            trigger: 'change'
+          }
+        ]
+      }
     }
   },
   methods: {
     onLogin () {
-      // 获取表单数据
-      const user = this.user
       // 表单数据
-
-      // 验证通过
+      this.$refs['login-form'].validate((valid, err) => {
+        if (!valid) {
+          return
+        }
+        this.login()
+      })
+    },
+    login () {
       this.loginLoading = true
-      request({
-        method: 'POST',
-        url: '/mp/v1_0/authorizations',
-        data: user
-      }).then(res => {
+      login(this.user).then(res => {
         console.log(res)
         this.$message({
           message: '登录成功',
           type: 'success'
         })
         this.loginLoading = false
+
+        this.$router.push('/')
       }).catch(err => {
         console.log('登录失败', err)
         this.$message.error('登录失败，手机号或验证码错误')
         this.loginLoading = false
+        this.$router.push('/')
       })
     }
+
   }
 }
 </script>
